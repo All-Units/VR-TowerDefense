f09e885962cedfcf8f0f5b4287b22ac4ab7cd24c
Zachary Day-Scott <zdayscott@gmail.com>
2023-08-12 14:48:56 -0700

    Probable cannon hit vfx

--- Assets/Project/Towers/Scripts/AOEProjectile.cs
+++ Assets/Project/Towers/Scripts/AOEProjectile.cs
@@ -1,10 +1,10 @@
-﻿using System.Linq;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class AOEProjectile : Projectile
 {
     public float splashRadius = 5;
-    
+    public GameObject hitParticles;
+
     protected override void OnCollision(Collision other)
     {
         var hits = Physics.OverlapSphere(other.collider.transform.position, splashRadius, LayerMask.GetMask("Enemy"));
@@ -17,6 +17,12 @@ public class AOEProjectile : Projectile
             {
                 healthController.TakeDamage(damage);
             }
+        }
+
+        if(hitParticles)
+        {
+            var particles = Instantiate(hitParticles, other.collider.transform.position, Quaternion.identity);
+            Destroy(particles, 2f);
         }
         
         isDestroying = true;
