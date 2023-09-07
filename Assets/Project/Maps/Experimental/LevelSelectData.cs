99311a2b9d8ca181646ac39c287f7aaeea6ee268
Zach <zdayscott@gmail.com>
2023-09-06 21:44:32 -0700

    Level select main menu map v1

---
+++ Assets/Project/Maps/Experimental/LevelSelectData.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(menuName = "SO/Level Select Data", fileName = "New Level Data")]
+public class LevelSelectData : ScriptableObject
+{
+    public int sceneToLoad;
+    public string title;
+}
