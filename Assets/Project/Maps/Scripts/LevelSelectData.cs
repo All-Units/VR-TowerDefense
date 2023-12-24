0ebd734534a3f73aca1ffa0f118a0c98027d8ee9
DanyeWestinator <44176712+DanyeWestinator@users.noreply.github.com>
2023-12-23 16:32:21 -0800

    Refactored main menu selectors

---
+++ Assets/Project/Maps/Scripts/LevelSelectData.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(menuName = "SO/Level Select Data", fileName = "New Level Data")]
+public class LevelSelectData : ScriptableObject
+{
+    public int sceneToLoad;
+    public string sceneName;
+    public string title;
+}
