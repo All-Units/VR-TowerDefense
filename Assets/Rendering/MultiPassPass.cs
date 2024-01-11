d587963ca76b49e4131e0414b250cd489c38f0ba
Zach <zdayscott@gmail.com>
2024-01-11 15:43:10 -0800

    Spent way too much time working on a global outline Render feature. Unfortunately found that the soft edges hide our bad frame rate, so I scraped it...

---
+++ Assets/Rendering/MultiPassPass.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.Universal;
+
+public class MultiPassPass : ScriptableRenderPass
+{
+    
+    
+    public override void Execute(ScriptableRenderContext context, ref RenderingData renderingData)
+    {
+        throw new System.NotImplementedException();
+    }
+}
