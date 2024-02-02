930dfc2e8166f63d2e1326fa16b7e793916c7e3d
Zach <zdayscott@gmail.com>
2024-02-02 14:35:19 -0800

    Gatling Haptics

---
+++ Assets/Project/Player/Interactables/Bow and Arrow/HapticController.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class HapticController : MonoBehaviour
+{
+    [SerializeField] private float amp, dur;
+    
+    private List<ActionBasedController> _controllers = new List<ActionBasedController>();
+    
+    public void SelectEnter(SelectEnterEventArgs args)
+    {
+            var transformParent = args.interactorObject?.transform?.parent;
+            if(transformParent == null) return;
+            
+            if(transformParent.TryGetComponent(out ActionBasedController actionBasedController))
+                _controllers.Add(actionBasedController);
+    }   
+    
+    public void SelectExit(SelectExitEventArgs args)
+    {
+        var transformParent = args.interactorObject?.transform?.parent;
+        if(transformParent == null) return;
+            
+        if(transformParent.TryGetComponent(out ActionBasedController actionBasedController))
+            _controllers.Remove(actionBasedController);
+    }
+
+    public void SendHapticPulse(float amplitude, float duration)
+    {
+        foreach (var controller in _controllers)
+        {
+            controller.SendHapticImpulse(amplitude, duration);
+        }
+    }    
+    
+    public void SendHapticPulse()
+    {
+        foreach (var controller in _controllers)
+        {
+            controller.SendHapticImpulse(amp, dur);
+        }
+    }
+}
