5346b46139a21f90ea0b2f3f4e4ba8f8c76d86a3
Zach <zdayscott@gmail.com>
2024-05-01 16:57:08 -0700

    Prebuild fixes

--- Assets/Project/Stats/StatTrackerHolder.cs
+++ Assets/Project/Stats/StatTrackerHolder.cs
@@ -7,5 +7,5 @@ public class StatTrackerHolder : ScriptableObject
 {
     public List<StatTracker> trackers = new();
 
-    public static StatTrackerHolder BaseStats => AssetDatabase.LoadAssetAtPath<StatTrackerHolder>("Assets/Project/Stats/SO/Trackers/Base Stats.asset");
+    public static StatTrackerHolder BaseStats => Resources.Load<StatTrackerHolder>("Stats/Base Stats.asset");
 }
